82fc40c152c1a3d2c72ef1802f7f9eea5ab66c45
adnanMem93 <adnan.mem.93@gmail.com>
2021-09-15 12:41:00 +0200

    Created folder ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+// Only change code below this line
+// Single line comment
+/* 
+Multi-line comments
+Multi-line comments
+Multi-line comments
+Multi-line comments
+Multi-line comments
+*/
+// Only change code above this line
+    
+}
+    
+main();
+    
+module.exports = main;
+    
